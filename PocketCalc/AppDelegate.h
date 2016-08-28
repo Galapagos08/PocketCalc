aba50e95326d37c09c63b9d6c2837dc51ac8139b
Dan Esrey <danesrey@dans-macbook-air.local>
2016-08-28 10:35:35 -0400

    Initial Commit

---
+++ PocketCalc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PocketCalc
+//
+//  Created by Dan Esrey on 2016/28/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PocketCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PocketCalc
+//
+//  Created by Dan Esrey on 2016/28/08.
+//  Copyright © 2016 Dan Esrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
